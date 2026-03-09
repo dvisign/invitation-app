0ae6d4da85f13369ee03732030b3e3c1c2dd2b9d
Jungmch-dev <jungmc.dev@watanow.com>
2026-03-10 08:42:59 +0900

    텍스트교체

--- src/components/section/Main/index.tsx
+++ src/components/section/Main/index.tsx
@@ -1,30 +1,30 @@
 import {
-  MainSectionStyles,
-  BrightWrapperStyles,
-  BrightContentStyles,
+	MainSectionStyles,
+	BrightWrapperStyles,
+	BrightContentStyles,
 } from "./styles";
 
 const MainSection = () => {
-  return (
-    <MainSectionStyles>
-      <BrightWrapperStyles>
-        <BrightContentStyles>
-          <div className="text-wrapper">
-            <h1>
-              박흠수 아버님
-              <br />
-              고희연에 초대합니다.
-            </h1>
-            <p>2026.3.21(토) 오후 12:00</p>
-            {/* <p>
+	return (
+		<MainSectionStyles>
+			<BrightWrapperStyles>
+				<BrightContentStyles>
+					<div className="text-wrapper">
+						<h1>
+							박흠수 아버지
+							<br />
+							고희연에 초대합니다.
+						</h1>
+						<p>2026.3.21(토) 오후 12:00</p>
+						{/* <p>
               충청북도 충주시 첨단산업4로 54
               <br /> 중앙하이츠 130동 1304호
             </p> */}
-          </div>
-        </BrightContentStyles>
-      </BrightWrapperStyles>
-    </MainSectionStyles>
-  );
+					</div>
+				</BrightContentStyles>
+			</BrightWrapperStyles>
+		</MainSectionStyles>
+	);
 };
 
 export default MainSection;
