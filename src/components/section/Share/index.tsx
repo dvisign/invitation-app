724c73f7b2a74e1a63e5c80d032f0000baa8593c
dvisign <jmch0922@gmail.com>
2026-03-10 00:07:40 +0900

    공유링크수정

--- src/components/section/Share/index.tsx
+++ src/components/section/Share/index.tsx
@@ -8,13 +8,12 @@ const Share = () => {
   const onSendLink = useCallback(() => {
     const content = {
       title:
-        "저희 두 사람이 하나되려 합니다.\n여러분의 축복이 더해진다면\n더욱 눈부신 날이 될 것 같습니다.",
+        "저희 아버님께서 칠순을 맞이하셨습니다. 평소 베풀어주신 사랑과 은혜에 감사드리며\n조촐하게나마 축하의 잔치를 마련했습니다.\n다들 바쁘시겠지만 우리 가족 모두 함께하는 자리가 되었으면 합니다. :)",
       description: "",
-      imageUrl:
-        "https://invitation-theta.vercel.app/img/invitation/3/3_main.jpg",
+      imageUrl: "https://invitation-app-theta.vercel.app/share.jpg",
       link: {
-        mobileWebUrl: "https://invitation-theta.vercel.app/invitation/3",
-        webUrl: "https://invitation-theta.vercel.app/invitation/3",
+        mobileWebUrl: "https://invitation-app-theta.vercel.app/",
+        webUrl: "https://invitation-app-theta.vercel.app/",
       },
       imageWidth: 800,
       imageHeight: 800,
@@ -22,8 +21,8 @@ const Share = () => {
     const buttons = {
       title: "청첩장 보기",
       link: {
-        mobileWebUrl: "https://invitation-theta.vercel.app/invitation/3",
-        webUrl: "https://invitation-theta.vercel.app/invitation/3",
+        mobileWebUrl: "https://invitation-app-theta.vercel.app/",
+        webUrl: "https://invitation-app-theta.vercel.app/",
       },
     };
     // @ts-expect-error: Kakao 전역 객체 타입 누락 무시
