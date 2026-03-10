69e17c35019d3e8b4f17e3036fe6b6c128c6406c
Jungmch-dev <jungmc.dev@watanow.com>
2026-03-10 09:07:36 +0900

    인트로 텍스트 및 워딩 변경

--- src/components/section/Location/index.tsx
+++ src/components/section/Location/index.tsx
@@ -1,103 +1,106 @@
-import {useCallback, useMemo} from "react";
-import {Map, MapMarker} from "react-kakao-maps-sdk";
-import {FaMapMarker} from "react-icons/fa";
-import {LocationWrapperStyles, MapContainer, LocationBtns} from "./styles";
+import { useCallback, useMemo } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { FaMapMarker } from "react-icons/fa";
+import { LocationWrapperStyles, MapContainer, LocationBtns } from "./styles";
 
 const Location = () => {
-  const location = {lat: 36.98889759894559, lng: 127.82565696643323};
-  const isMobile = useMemo(() => {
-    // 서버 측 렌더링 시 navigator 객체 부재로 인한 에러 방어
-    if (typeof navigator === "undefined") return false;
+	const location = { lat: 36.98889759894559, lng: 127.82565696643323 };
+	const isMobile = useMemo(() => {
+		// 서버 측 렌더링 시 navigator 객체 부재로 인한 에러 방어
+		if (typeof navigator === "undefined") return false;
 
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent,
-    );
-  }, []);
-  const naver = () => {
-    window.open(
-      "http://map.naver.com/?menu=location&pinType=place&lat=36.98889759894559&lng=127.82565696643323&title=충주 중앙하이츠",
-    );
-  };
-  const kakaoNavi = useCallback(() => {
-    if (!isMobile) {
-      window.open(
-        `https://map.kakao.com/link/map/충주 중앙하이츠,${location.lat},${location.lng}`,
-        "PopupWin",
-        "width=1160, height=600",
-      );
-    } else {
-      try {
-        // @ts-expect-error: Kakao 전역 객체 타입 누락 무시
-        window.Kakao.Navi.start({
-          name: "충주 중앙하이츠",
-          x: 127.82565696643323,
-          y: 36.98889759894559,
-          coordType: "wgs84",
-        });
-      } catch (e: unknown) {
-        if (e instanceof Error) {
-          // 조건문을 통과하면 여기서부터 e는 완벽한 Error 타입으로 추론됨
-          alert(`오류: ${e.message}`);
-        } else {
-          // Error 객체가 아닌 다른 무언가가 던져졌을 때의 예외 처리
-          alert("알 수 없는 오류가 발생했습니다.");
-        }
-      }
-    }
-  }, []);
+		return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+			navigator.userAgent,
+		);
+	}, []);
+	const naver = () => {
+		window.open(
+			"http://map.naver.com/?menu=location&pinType=place&lat=36.98889759894559&lng=127.82565696643323&title=충주 중앙하이츠",
+		);
+	};
+	const kakaoNavi = useCallback(() => {
+		if (!isMobile) {
+			window.open(
+				`https://map.kakao.com/link/map/충주 중앙하이츠,${location.lat},${location.lng}`,
+				"PopupWin",
+				"width=1160, height=600",
+			);
+		} else {
+			try {
+				// @ts-expect-error: Kakao 전역 객체 타입 누락 무시
+				window.Kakao.Navi.start({
+					name: "충주 중앙하이츠",
+					x: 127.82565696643323,
+					y: 36.98889759894559,
+					coordType: "wgs84",
+				});
+			} catch (e: unknown) {
+				if (e instanceof Error) {
+					// 조건문을 통과하면 여기서부터 e는 완벽한 Error 타입으로 추론됨
+					alert(`오류: ${e.message}`);
+				} else {
+					// Error 객체가 아닌 다른 무언가가 던져졌을 때의 예외 처리
+					alert("알 수 없는 오류가 발생했습니다.");
+				}
+			}
+		}
+	}, []);
 
-  return (
-    <LocationWrapperStyles>
-      <h2>Location</h2>
-      <p>
-        충청북도 충주시 첨단산업4로 54
-        <br /> 중앙하이츠 130동 1304호
-      </p>
-      <MapContainer>
-        <Map
-          center={location}
-          style={{width: "100%", height: "100%"}}
-          level={3}
-          draggable={true}>
-          <MapMarker position={location} />
-        </Map>
-      </MapContainer>
-      <LocationBtns>
-        <div className="map_control_btns">
-          <button
-            type="button"
-            id="naverMapsBtn"
-            className="mapBtns"
-            onClick={() => naver()}>
-            <div>
-              <FaMapMarker className="markerIcons" />
-            </div>
-            <p>
-              네이버
-              <br />
-              지도
-            </p>
-          </button>
-        </div>
-        <div className="map_control_btns">
-          <button
-            type="button"
-            id="kakaoMapsBtn"
-            className="mapBtns"
-            onClick={kakaoNavi}>
-            <div>
-              <FaMapMarker className="markerIcons" />
-            </div>
-            <p>
-              카카오
-              <br />
-              지도
-            </p>
-          </button>
-        </div>
-      </LocationBtns>
-    </LocationWrapperStyles>
-  );
+	return (
+		<LocationWrapperStyles>
+			<h2>Location</h2>
+			<p>
+				충청북도 충주시 첨단산업4로 54
+				<br /> 중앙하이츠 103동 1304호
+			</p>
+			<MapContainer>
+				<Map
+					center={location}
+					style={{ width: "100%", height: "100%" }}
+					level={3}
+					draggable={true}
+				>
+					<MapMarker position={location} />
+				</Map>
+			</MapContainer>
+			<LocationBtns>
+				<div className="map_control_btns">
+					<button
+						type="button"
+						id="naverMapsBtn"
+						className="mapBtns"
+						onClick={() => naver()}
+					>
+						<div>
+							<FaMapMarker className="markerIcons" />
+						</div>
+						<p>
+							네이버
+							<br />
+							지도
+						</p>
+					</button>
+				</div>
+				<div className="map_control_btns">
+					<button
+						type="button"
+						id="kakaoMapsBtn"
+						className="mapBtns"
+						onClick={kakaoNavi}
+					>
+						<div>
+							<FaMapMarker className="markerIcons" />
+						</div>
+						<p>
+							카카오
+							<br />
+							지도
+						</p>
+					</button>
+				</div>
+			</LocationBtns>
+		</LocationWrapperStyles>
+	);
 };
 
 export default Location;
--- src/components/section/Share/index.tsx
+++ src/components/section/Share/index.tsx
@@ -1,48 +1,48 @@
-import {useCallback} from "react";
+import { useCallback } from "react";
 
-import {RiKakaoTalkFill} from "react-icons/ri";
+import { RiKakaoTalkFill } from "react-icons/ri";
 
-import {ShareWrapper} from "./styles";
+import { ShareWrapper } from "./styles";
 
 const Share = () => {
-  const onSendLink = useCallback(() => {
-    const content = {
-      title:
-        "저희 아버님께서 칠순을 맞이하셨습니다. 평소 베풀어주신 사랑과 은혜에 감사드리며\n조촐하게나마 축하의 잔치를 마련했습니다.\n다들 바쁘시겠지만 우리 가족 모두 함께하는 자리가 되었으면 합니다. :)",
-      description: "",
-      imageUrl: "https://invitation-app-theta.vercel.app/share.jpg",
-      link: {
-        mobileWebUrl: "https://invitation-app-theta.vercel.app",
-        webUrl: "https://invitation-app-theta.vercel.app",
-      },
-      imageWidth: 800,
-      imageHeight: 800,
-    };
-    const buttons = {
-      title: "초대장 보기",
-      link: {
-        mobileWebUrl: "https://invitation-app-theta.vercel.app",
-        webUrl: "https://invitation-app-theta.vercel.app",
-      },
-    };
-    // @ts-expect-error: Kakao 전역 객체 타입 누락 무시
-    Kakao.Link.sendDefault({
-      objectType: "feed",
-      content: content,
-      buttons: [buttons],
-    });
-  }, []);
-  return (
-    <ShareWrapper id="share_section" className="pb-5">
-      <div>
-        <button id="kakaoShereBtn" onClick={() => onSendLink()}>
-          <span id="kakaoShareIcons">
-            <RiKakaoTalkFill />
-          </span>
-          <span id="kakaoShareText">카카오톡 공유하기</span>
-        </button>
-      </div>
-    </ShareWrapper>
-  );
+	const onSendLink = useCallback(() => {
+		const content = {
+			title:
+				"저희 아버님께서 칠순을 맞이하셨습니다. 평소 베풀어주신 사랑과 은혜에 감사드리며\n조촐하게나마 가족식사 모임 자리를 마련했습니다.\n다들 바쁘시겠지만 우리 가족 모두 함께하는 자리가 되었으면 합니다. :)",
+			description: "",
+			imageUrl: "https://invitation-app-theta.vercel.app/share.jpg",
+			link: {
+				mobileWebUrl: "https://invitation-app-theta.vercel.app",
+				webUrl: "https://invitation-app-theta.vercel.app",
+			},
+			imageWidth: 800,
+			imageHeight: 800,
+		};
+		const buttons = {
+			title: "초대장 보기",
+			link: {
+				mobileWebUrl: "https://invitation-app-theta.vercel.app",
+				webUrl: "https://invitation-app-theta.vercel.app",
+			},
+		};
+		// @ts-expect-error: Kakao 전역 객체 타입 누락 무시
+		Kakao.Link.sendDefault({
+			objectType: "feed",
+			content: content,
+			buttons: [buttons],
+		});
+	}, []);
+	return (
+		<ShareWrapper id="share_section" className="pb-5">
+			<div>
+				<button id="kakaoShereBtn" onClick={() => onSendLink()}>
+					<span id="kakaoShareIcons">
+						<RiKakaoTalkFill />
+					</span>
+					<span id="kakaoShareText">카카오톡 공유하기</span>
+				</button>
+			</div>
+		</ShareWrapper>
+	);
 };
 export default Share;
